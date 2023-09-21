09baa83865046684c51b012bbc7c9ce60af7a6ef
root <root@DESKTOP-8C1TN5E.localdomain>
2023-09-21 03:04:47 -0400

    feat: implemented players turn logic as a function and display game state

--- bin/game.mjs
+++ bin/game.mjs
@@ -39,7 +39,7 @@ const initializeBeforePlay = () => {
 
     } else { 
         //if filename not provided 
-        let hands = [], starter = {}; 
+        let hands = []; 
 
         //generate a deck and shuffle the deck
         deck = shuffle(generateDeck());
@@ -55,20 +55,18 @@ const initializeBeforePlay = () => {
             let drawnCards;
             ([deck, drawnCards] = draw(deck)); //draws one card by default. Returns updated deck and array of cards drawn
            
-            starter = drawnCards[0];
+            nextPlay = drawnCards[0];
 
-            if (starter["rank"] === EIGHT) {
-                discardPile.push(starter); 
+            if (nextPlay["rank"] === EIGHT) {
+                discardPile.push(nextPlay); 
             }
-        } while(starter["rank"] === EIGHT); 
-
-        nextPlay = starter; 
+        } while(nextPlay["rank"] === EIGHT); 
 
         playGame(deck, playerHand, computerHand, discardPile, nextPlay); 
     }
 }
 
-const displayState = (nextPlay, discardPile, deck) => {
+const displayState = (deck, playerHand, computerHand, discardPile, nextPlay) => {
     let text = "CR🤪ZY 8's"; 
     const screenWidth = process.stdout.columns;
     const padding = Math.floor((screenWidth/2 - text.length) / 2);
@@ -79,16 +77,76 @@ const displayState = (nextPlay, discardPile, deck) => {
     // Log the centered text to the console
     console.log(`\n${centeredText}\n${LINE_SEP}`);
     console.log(`Next suit/rank to play: ➡️  ${nextPlay["rank"]}${nextPlay["suit"]}  ⬅️\n${LINE_SEP}`); 
-    let top = 
+
     console.log(`Top of discard pile: ${discardPile.length > 0 ? (discardPile[discardPile.length - 1]["rank"] + discardPile[discardPile.length - 1]["suit"]): "empty"}\nNumber of cards left in deck: ${deck.length}\n${LINE_SEP}`); 
+
+    console.log(`🤖 ✋ (computer hand): ${handToString(computerHand)}`); 
+    console.log(`😊 ✋ (player hand): ${handToString(playerHand)}`); 
+    console.log(LINE_SEP); 
+}
+
+const playerTurn = (playerHand, nextPlay, deck, discardPile) => { //IS DECK PASSED BY REF?
+    console.log("😊 Player's turn...");  
+    let match = false; 
+    for (let i = 0; i < playerHand.length; i++)
+    {
+        if (matchesAnyProperty(nextPlay, playerHand[i]) === true){
+            console.log("Enter the number of the card you would like to play:"); 
+            match = true;  
+            break; 
+        }
+    }
+    if (match === true)
+    {
+        console.log(handToString(playerHand, '\n', true));
+        //get input from user
+    } else{
+        console.log(`😔 You have no playable cards\nPress ENTER to draw cards until matching: ${nextPlay["rank"]}, ${nextPlay["suit"]}, ${EIGHT}`);
+        /* 
+        //
+        //
+        //
+        //  GET USER'S INPUT - SHOULD PRESS ENTER HERE
+        //
+        //
+        //
+        */
+        //IF USER INPUTS ENTER - IF statement HERE
+        {
+            let removed;
+            ([deck, removed] = drawUntilPlayable(deck, nextPlay)); 
+
+            console.log(`Cards drawn: ${handToString(removed)}`); 
+            console.log(`Card played: ${removed[removed.length-1]["rank"]}${removed[removed.length-1]["suit"]}`); 
+            
+            //update discard pile CHECK IF AUTO UPDATES
+            discardPile.push(...removed); 
+
+             /* 
+            //
+            //
+            //
+            //  GET USER'S INPUT - SHOULD PRESS ENTER HERE to continue
+            //
+            //  make function eightPlayed(); if easy 
+            //
+            */
+        }
+
+    }
+    //MAYBE SHOULD RETURN DECK IF DECK ISNT AUTO UPDATED OUTSIDE THIS FUNCTION
+    //either return removed and push to discard pile or update discardpile and return
 }
 
 const playGame = (deck, playerHand, computerHand, discardPile, nextPlay) => {
-     //([deck, playerHand, computerHand, discardPile, nextPlay] = initializeBeforePlay()); 
-     console.log(nextPlay);
-     displayState(nextPlay, discardPile, deck);
+    displayState(deck, playerHand, computerHand, discardPile, nextPlay);
+    playerTurn(playerHand, nextPlay, deck, discardPile); 
 }
 
 initializeBeforePlay(); 
 
 
+//3
+/*{"suit": "❤️", "rank": "2"}, //3
+        {"suit": "❤️", "rank": "4"},
+        {"suit": "♠️", "rank": "3"} */
