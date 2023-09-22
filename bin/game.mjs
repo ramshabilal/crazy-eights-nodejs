d3cb33eac3dfe18841243e53f0dbd1ab677abdb1
root <root@DESKTOP-8C1TN5E.localdomain>
2023-09-21 22:17:34 -0400

    added comments

--- bin/game.mjs
+++ bin/game.mjs
@@ -13,56 +13,59 @@ import {readFile} from 'fs';
 const EIGHT = '8'; 
 const LINE_SEP = "-----------------------------------------------------------------------------------"; 
 
-
-
+//function to display state of game
 const displayState = (deck, playerHand, computerHand, discardPile, nextPlay) => {
     const text = "CR🤪ZY 8's"; 
+
+    //center title toward the middle of the screen (not exactly middle)
     const screenWidth = process.stdout.columns;
     const padding = Math.floor((screenWidth/2 - text.length) / 2);
   
-    // Create a string with padding spaces on both sides
+    // create a string with padding spaces on both sides
     const centeredText = ' '.repeat(padding) + text + ' '.repeat(padding);
   
-    // Log the centered text to the console
+    // log the centered text to the console
     console.log(`\n${centeredText}\n${LINE_SEP}`);
     console.log(`Next suit/rank to play: ➡️  ${nextPlay["rank"]}${nextPlay["suit"]}  ⬅️\n${LINE_SEP}`); 
     console.log(`Top of discard pile: ${discardPile.length > 0 ? (discardPile[discardPile.length - 1]["rank"] + discardPile[discardPile.length - 1]["suit"]): nextPlay["rank"]+nextPlay["suit"]}\nNumber of cards left in deck: ${deck.length}\n${LINE_SEP}`); 
-
     console.log(`🤖 ✋ (computer hand): ${handToString(computerHand)}`); 
     console.log(`😊 ✋ (player hand): ${handToString(playerHand)}`); 
     console.log(LINE_SEP); 
 };
 
+//function for the player's turn
 const playerTurn = (playerHand, nextPlay, deck, discardPile) => { 
     console.log("😊 Player's turn...");  
+
+    //check if any card's rank/suit in the player's hand matches the rank/suit of nextPlay or if player has an 8
     let match = false; 
     for (let i = 0; i < playerHand.length; i++)
     {
         if (matchesAnyProperty(nextPlay, playerHand[i]) === true || playerHand[i]["rank"]===EIGHT){
-            match = true;  
+            match = true;  //if a card matches, turn match flag true
             break; 
         }
     }
 
-    let cardPlayed; 
+    let cardPlayed; //to store the card the player plays
+    //if a card matches, give player option to choose the card they want to play
     if (match === true)
     {
-        //get input from user
+        //get input from user and update hand, discardPile, etc... accordingly
         const num = question(`Enter the number of the card you would like to play:\n${handToString(playerHand, '\n', true)}\n`);
+
         cardPlayed = playerHand[num-1];
+
         console.log(`Card played: ${cardPlayed["rank"]}${cardPlayed["suit"]}`); 
 
-        discardPile.push(cardPlayed);
-        //console.log(playerHand);
-
-        playerHand.splice(num - 1, 1); //remove from player hand
-        // console.log(playerHand);
-
+        discardPile.push(cardPlayed); //add to discard pile 
+        playerHand.splice(num - 1, 1); //remove the played card from player hand
         nextPlay = cardPlayed; //the card played becomes next card to be played i.e. starter
+
         question("Press ENTER to continue");
 
-    } else{
-
+    } else {
+        //if no playable cards, draw until playable card found
         const string = `😔 You have no playable cards\nPress ENTER to draw cards until matching: ${nextPlay["rank"]}, ${nextPlay["suit"]}, ${EIGHT}`;
         question(string); 
         
@@ -74,24 +77,29 @@ const playerTurn = (playerHand, nextPlay, deck, discardPile) => {
         
         cardPlayed = removed[removed.length-1];
         nextPlay = cardPlayed;
+
+        //add the card played to the discard pile
         discardPile.push(cardPlayed); 
-        removed.pop();
-        //update discard pile CHECK IF AUTO UPDATES
-        playerHand.push(...removed); 
+
+        //add the drawn cards (other than the card played) to player's hand
+        removed.pop(); //remove cardPlayed first 
+        playerHand.push(...removed); //then add to playerHand
         //console.log(discardPile);
          
         question("Press ENTER to continue"); 
     }
+    //if player plays an eight, give option to choose new suit and assign accordingly to nextPlay
     const suits = ['♠️', '❤️', '♣️', '♦️'];
     if (cardPlayed["rank"] === EIGHT){
         const suit = question(`CRAZY EIGHTS! You played an 8 - choose a suit\n1: ${suits[0]}\n2: ${suits[1]}\n3: ${suits[2]}\n4: ${suits[3]}\n `);
         
-        nextPlay = {...nextPlay, "suit":suits[suit-1]};
+        nextPlay = {...nextPlay, "suit":suits[suit-1]}; //creating new nextPlay and reassigning to ensure that top of discard pile does not change 
        
         console.log(`You chose to set the suit to ${suits[suit-1]}`);
         question("Press ENTER to continue");
     }
 
+    //return updated data to the main game
     const retArray = [];
     retArray.push(playerHand);
     retArray.push(nextPlay);
@@ -101,10 +109,15 @@ const playerTurn = (playerHand, nextPlay, deck, discardPile) => {
     return retArray; 
 };
 
+//similar process as playerTurn
 const computerTurn = (computerHand, nextPlay, deck, discardPile) => {
+
     console.log("🤖 Computer's turn..."); 
     let match = false; 
     let cardPlayed; 
+
+    //logic similar to playerTurn except that 
+    //first matching card (matching rank/suit or eight) found is played
     for (let i = 0; i < computerHand.length; i++)
     {
         if (matchesAnyProperty(nextPlay, computerHand[i]) === true || computerHand[i]["rank"]===EIGHT){
@@ -114,27 +127,28 @@ const computerTurn = (computerHand, nextPlay, deck, discardPile) => {
             console.log(`Card played: ${cardPlayed["rank"]}${cardPlayed["suit"]}`); 
             computerHand.splice(i, 1); 
             discardPile.push(cardPlayed); 
-            break; //6
+            break; 
         }
     }
     if (match===false)
     {
         let removed;
-        ([deck, removed] = drawUntilPlayable(deck, nextPlay)); //should cards drawn go to discard pile or playerHand??????
+        ([deck, removed] = drawUntilPlayable(deck, nextPlay)); 
        
         console.log(`Cards drawn: ${handToString(removed)}`); 
         console.log(`Card played: ${removed[removed.length-1]["rank"]}${removed[removed.length-1]["suit"]}`); 
         
+        //update accordingly - similar to playerTurn
         cardPlayed = removed[removed.length-1];
-        //update discard pile CHECK IF AUTO UPDATES
         nextPlay = cardPlayed; 
         discardPile.push(cardPlayed); 
-        //console.log(discardPile);
         removed.pop();
         computerHand.push(...removed);
     }
+
     const suits = ['♠️', '❤️', '♣️', '♦️'];
     if (cardPlayed["rank"] === EIGHT){
+        //logic used: computer will always pick heart as suit
         console.log(`CRAZY EIGHTS! Computer played an 8 and chose the suit ${suits[1]}`);
         nextPlay = {...nextPlay, "suit":suits[1]}; 
     }
@@ -149,6 +163,8 @@ const computerTurn = (computerHand, nextPlay, deck, discardPile) => {
 
 const playGame = (deck, playerHand, computerHand, discardPile, nextPlay) => {
 
+    //display current state of game
+    //player turn then computer turn
     displayState(deck, playerHand, computerHand, discardPile, nextPlay);
 
     ([playerHand, nextPlay, deck, discardPile] = playerTurn(playerHand, nextPlay, deck, discardPile)); 
@@ -157,15 +173,20 @@ const playGame = (deck, playerHand, computerHand, discardPile, nextPlay) => {
     console.clear(); 
     displayState(deck, playerHand, computerHand, discardPile, nextPlay);
     question("Press ENTER to continue"); 
-    
+
     ([computerHand, nextPlay, deck, discardPile] = computerTurn(computerHand, nextPlay, deck, discardPile)); 
     question("Computer turn complete. Press ENTER to continue"); 
     console.clear(); 
     
+    //display final state before end of program
     displayState(deck, playerHand, computerHand, discardPile, nextPlay);
     
 };
 
+//function that checks if command has filename 
+//if yes, open the file
+//if file opens, play game by calling playGame() as callback
+//if no file fiven, use predefined cards
 const beginPlay = () => {
     const args = process.argv; 
     let deck, playerHand, computerHand, discardPile, nextPlay;
@@ -216,4 +237,5 @@ const beginPlay = () => {
     }
 };
 
+//call begin play to open file/get predefined cards and start playing game
 beginPlay(); 
